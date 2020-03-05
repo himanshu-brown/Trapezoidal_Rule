b4f4a5afd3844814096f12350bf9605316534d5e
Himanshu Sharma <54981812+himanshu-brown@users.noreply.github.com>
2020-03-05 20:08:06 +0530

    Add files via upload

---
+++ TRAPEZOI.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+//f(x)=1/sqrt(1+x*x)
+double fun(double x)
+{
+ return(1/(sqrt(1+x*x)));
+}
+int main()
+{
+ float x[6],y[6];
+ int i,l1,l2,n=5;
+ double h,sum=0,p;
+ clrscr();
+ printf("--------------------------TRAPEZOIDAL RULE--------------------------------\n\n");
+ printf("1/(sqrt(1+x*x))\n");
+ printf("Enter the lower limit : ");
+ scanf("%d",&l1);
+ printf("Enter the upper limit : ");
+ scanf("%d",&l2);
+ h=(l2-l1)/(float)n;
+ printf("h : %.3f\n",h);
+ p=l1;
+ for(i=0;i<=n;i++)
+ {
+  x[i]=p;
+  y[i]=fun(p);
+  p=p+h;
+ }
+ sum=y[0]+y[5];
+ for(i=1;i<n;i++)
+ {
+  sum=sum+2*y[i];
+ }
+ sum=sum*(h/2);
+ printf("Result : %lf",sum);
+ getch();
+}
